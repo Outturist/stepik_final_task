a88c7325b0d70fdb61473a10d0f59feac4124c09
Outturist <0ecpjuzphnun@mail.ru>
2020-05-30 12:00:06 +0300

    in test_product_page.py added parametr - different links

--- pages/locators.py
+++ pages/locators.py
@@ -5,7 +5,16 @@ class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
-
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLoacators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    ALERT_ADDED_TO_BASKET = (By.XPATH, '//div[@class="alert alert-safe alert-noicon alert-success  fade in"][1]')
+    ALERT_WITH_BASKET_COST = (By.XPATH, '//div[@class="alert alert-safe alert-noicon alert-info  fade in"]')
+    COST_IN_ALERT = (By.XPATH, '//div[@class="alert alert-safe alert-noicon alert-info  fade in"]//p/strong')
+    BOOK_NAME = (By.XPATH, '//div[@class="col-sm-6 product_main"]//h1')
+    BOOK_NAME_IN_ALLERT = (By.XPATH, '//div[@class="alert alert-safe alert-noicon alert-success  fade in"][1]//div//strong')
+    BOOK_PRICE = (By.XPATH, '//div[@class="col-sm-6 product_main"]//p[@class="price_color"]')
---
+++ pages/product_page.py
@@ -0,0 +1,32 @@
+from .base_page import BasePage
+from .locators import ProductPageLoacators
+
+class ProductPage(BasePage):
+    def test_guest_can_add_product_to_basket(self):
+        self.add_to_basket()
+        self.should_be_success_alert()
+        self.should_be_right_name_of_the_added_book_in_alert()
+        self.should_be_alert_with_basket_cost()
+        self.should_be_equal_book_price_and_cost_in_alert()
+
+    def add_to_basket(self):
+        add_to_basket_button = self.browser.find_element(*ProductPageLoacators.ADD_TO_BASKET_BUTTON)
+        add_to_basket_button.click()
+        self.solve_quiz_and_get_code()
+
+    def should_be_success_alert(self):
+        assert self.is_element_present(*ProductPageLoacators.ALERT_ADDED_TO_BASKET), "There isn't success alert"
+
+
+    def should_be_right_name_of_the_added_book_in_alert(self):
+        book_name = self.browser.find_element(*ProductPageLoacators.BOOK_NAME).text
+        book_name_in_allert = self.browser.find_element(*ProductPageLoacators.BOOK_NAME_IN_ALLERT).text
+        assert book_name == book_name_in_allert, f"book name in allert isn't right, expected: {book_name}, appeared: {book_name_in_allert}"
+
+    def should_be_alert_with_basket_cost(self):
+        assert self.is_element_present(*ProductPageLoacators.ALERT_WITH_BASKET_COST), "There isn't alert_with_basket_cost"
+
+    def should_be_equal_book_price_and_cost_in_alert(self):
+        cost_in_alert = self.browser.find_element(*ProductPageLoacators.COST_IN_ALERT).text
+        book_price = self.browser.find_element(*ProductPageLoacators.BOOK_PRICE).text
+        assert cost_in_alert == book_price, f"cost in alert isn't the same as book price, expected: {book_price}, appeared: {cost_in_alert}"
